0be39b4012a146f665c2e55e077e98b441277bcd
candlez <thomasmcnamara@vt.edu>
2026-06-27 17:29:28 -0400

    style navbar

--- src/app/components/nav-header/nav-header.component.ts
+++ src/app/components/nav-header/nav-header.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nav-header.component.html',
   styleUrl: './nav-header.component.scss',
 })
