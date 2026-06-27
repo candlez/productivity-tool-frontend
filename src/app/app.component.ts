24212dad36e6da9714bf607a13e54f9d988626eb
candlez <thomasmcnamara@vt.edu>
2026-06-27 12:24:51 -0400

    remove test service and functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpErrorResponse } from '@angular/common/http';
+import { Component,  } from '@angular/core';
 
-
-import { TestItem, TestService } from './services/test.service';
 
 @Component({
   selector: 'app-root',
@@ -10,36 +7,9 @@ import { TestItem, TestService } from './services/test.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  loaded: number = 2;
-  message: string = "";
-  items: TestItem[] = [];
+export class AppComponent {
 
-  constructor(public testService: TestService) {
+  constructor() {
 
-  }
-  
-  ngOnInit(): void {
-    this.testService.getTest1().subscribe({
-      next: (value: string) => {
-        this.message = value;
-        this.loaded--;
-      },
-      error: (error: HttpErrorResponse) => {
-        console.error(error);
-        this.message = error.message;
-        this.loaded--;
-      }
-    });
-    this.testService.getTest2().subscribe({
-      next: (values: TestItem[]) => {
-        this.items = values;
-        this.loaded--;
-      },
-      error: (error: HttpErrorResponse) => {
-        console.error(error);
-        this.loaded--;
-      }
-    });
   }
 }
