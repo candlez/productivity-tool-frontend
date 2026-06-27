5c5b5e03c6992f9db8fae482dd86eb875b6c2653
candlez <thomasmcnamara@vt.edu>
2026-06-27 16:24:55 -0400

    set up routing between components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component,  } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavHeaderComponent } from "./components/nav-header/nav-header.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet, NavHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "login", component: LoginPageComponent },
+    { path: "home", component: HomePageComponent },
+    { path: "", redirectTo: "/home", pathMatch: "full" },
+    { path: "**", component: NotFoundPageComponent }
+];
