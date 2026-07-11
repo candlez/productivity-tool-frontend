9056bdea68b46f4e27aae6b74489ec6bec2bfb7c
candlez <thomasmcnamara@vt.edu>
2026-07-11 13:43:20 -0400

    styled login page, added material

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import { MatButton } from "@angular/material/button";
+
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [
+    FormsModule, 
+    ReactiveFormsModule, 
+    MatLabel,
+    MatFormField,
+    MatInput,
+    MatButton
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
+  form: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      username: [''],
+      password: ['']
+    });
+  }
+
+  submitForm(): void {
+    console.log(this.form.value.username, this.form.value.password);
+  }
 }
